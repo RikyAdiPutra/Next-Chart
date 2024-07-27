d814cf87994266230473ff0fa22c6f01e771d4dd
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-07-27 17:19:32 +0700

    create NavbarSection

--- src/app/components/DashboardChart.jsx
+++ src/app/components/DashboardChart.jsx
@@ -1,5 +1,48 @@
+const BottomSection = () => {
+  return <div>BOTTOM SECTION</div>;
+};
+
+const RightSection = () => {
+  return <div>RIGHT SECTION</div>;
+};
+
+const ChartDetail = () => {
+  return <div>CHART DETAIL</div>;
+};
+
+const SideBar = () => {
+  return <div>SIDEBAR</div>;
+};
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-400 p-4 flex justify-between">
+      <div className="w-[120px] h-[40px] bg-gray-700 rounded-lg"></div>
+      <div className="flex gap-2 items-end">
+        <div className="w-[25px] h-[25px] rounded-full bg-gray-700"></div>
+        <div className="w-[25px] h-[25px] rounded-full bg-gray-700"></div>
+        <div className="w-[35px] h-[35px] rounded-full bg-gray-700"></div>
+      </div>
+    </div>
+  );
+};
+
 const ChartDasboard = () => {
-  return <div>ChartDasboard</div>;
+  return (
+    <div className="max-w-[1100px] mx-auto">
+      <Navbar />
+      {/* <main className="flex">
+        <SideBar />
+        <div>
+          <div className="flex">
+            <ChartDetail />
+            <RightSection />
+          </div>
+          <BottomSection />
+        </div>
+      </main> */}
+    </div>
+  );
 };
 
 export default ChartDasboard;
